beeaa55fce64fb93d94206769432442132100b1f
Deepjyoti Barman <deepjyoti@supertokens.com>
2025-09-01 12:41:42 +0530

    feat: migrate the tenants BE repo and update various things

---
+++ shared/tenants/src/types.ts
@@ -0,0 +1,38 @@
+import { TenantConfig } from 'supertokens-node/lib/build/recipe/multitenancy/types';
+
+export type TenantJoinData = {
+  tenantId: string;
+};
+
+export type TenantCreateData = {
+  name: string;
+};
+
+export type TenantDetails = {
+  tenantId: string;
+} & TenantConfig;
+
+export type TenantList = {
+  tenants: TenantDetails[];
+  joinedTenantIds: string[];
+};
+
+export type InviteeDetails = {
+  email: string;
+  role: string;
+  code: string;
+};
+
+export type TenantCreationRequest = {
+  name: string;
+  firstFactors?: string[] | null;
+  requestId: string;
+};
+
+export type TenantMetadata = {
+  invitees: InviteeDetails[];
+};
+
+export type TenantCreationRequestMetadata = {
+  requests: (TenantCreationRequest & { userId: string })[];
+};
