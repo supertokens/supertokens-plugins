bc245256cfc37fb512c7e3eef7f16c9c0b8b54e3
Deepjyoti Barman <deepjyoti@supertokens.com>
2025-09-08 16:21:41 +0530

    fix: styling issues with wa tab components

--- packages/tenants-react/src/components/tab/TenantTab.tsx
+++ packages/tenants-react/src/components/tab/TenantTab.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 type TenantTabProps = {
-    description: string;
-    children: React.ReactNode;
-}
+  description: string;
+  children: React.ReactNode;
+};
 
 export const TenantTab: React.FC<TenantTabProps> = ({ description, children }) => {
   return (
     <div>
-        <div>{description}</div>
-        <div>{children}</div>
+      <div>{description}</div>
+      <div>{children}</div>
     </div>
   );
 };
--- packages/tenants-react/vite.config.ts
+++ packages/tenants-react/vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import * as path from 'path';
-import packageJson from './package.json';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import * as path from "path";
+import packageJson from "./package.json";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig(() => {
   return {
     root: __dirname,
     plugins: [
       react(),
-      dts({ entryRoot: 'src', tsconfigPath: path.join(__dirname, 'tsconfig.json') }),
+      dts({ entryRoot: "src", tsconfigPath: path.join(__dirname, "tsconfig.json") }),
       peerDepsExternal(),
+      cssInjectedByJsPlugin(),
     ],
 
     build: {
-      outDir: 'dist',
+      outDir: "dist",
       sourcemap: false,
       emptyOutDir: true,
       commonjsOptions: {
@@ -23,12 +25,12 @@ export default defineConfig(() => {
       },
       lib: {
         // Could also be a dictionary or array of multiple entry points.
-        entry: 'src/index.ts',
-        fileName: 'index',
+        entry: "src/index.ts",
+        fileName: "index",
         name: packageJson.name,
         // Change this to the formats you want to support.
         // Don't forget to update your package.json as well.
-        formats: ['es' as const, 'cjs' as const],
+        formats: ["es" as const, "cjs" as const],
       },
       rollupOptions: {
         cache: false,
