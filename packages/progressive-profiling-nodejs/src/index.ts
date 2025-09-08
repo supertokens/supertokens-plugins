b581221c7c5cff3842f0d08f0583c4f50afa5d5f
Victor Bojica <victor@niftylearning.io>
2025-09-08 14:04:15 +0300

    export get set profile methods

--- packages/progressive-profiling-nodejs/src/index.ts
+++ packages/progressive-profiling-nodejs/src/index.ts
@@ -1,7 +1,22 @@
 import { init } from "./plugin";
 import { PLUGIN_ID, PLUGIN_VERSION } from "./constants";
+import { Implementation } from "./implementation";
+import { SessionContainerInterface } from "supertokens-node/recipe/session/types";
+import { ProfileFormData } from "@supertokens-plugins/progressive-profiling-shared";
 
 export type { RegisterSections as RegisterSection } from "./types";
 
-export { init, PLUGIN_ID, PLUGIN_VERSION };
-export default { init, PLUGIN_ID, PLUGIN_VERSION };
+const getProfile = (session: SessionContainerInterface, userContext?: Record<string, any>) => {
+  return Implementation.getInstanceOrThrow().getSectionValues(session, userContext);
+};
+
+const setProfile = (
+  session: SessionContainerInterface,
+  profile: ProfileFormData,
+  userContext?: Record<string, any>,
+) => {
+  return Implementation.getInstanceOrThrow().setSectionValues(session, profile, userContext);
+};
+
+export { init, PLUGIN_ID, PLUGIN_VERSION, getProfile, setProfile };
+export default { init, PLUGIN_ID, PLUGIN_VERSION, getProfile, setProfile };
