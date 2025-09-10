2db06a825403416207ea94762548c84c7d68a3bb
Deepjyoti Barman <deepjyoti@supertokens.com>
2025-09-10 12:36:14 +0530

    feat: add support for user roles in users in tenants tab

--- packages/tenants-react/src/components/users/TenantUsers.tsx
+++ packages/tenants-react/src/components/users/TenantUsers.tsx
@@ -1,7 +1,11 @@
 import classNames from "classnames/bind";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Session from "supertokens-auth-react/recipe/session";
 import { User } from "supertokens-web-js/types";
 
+import { ROLES } from "../../../../../shared/tenants/src/roles";
+import { SelectInput } from "../../../../../shared/ui/src/components";
+import { logDebugMessage } from "../../logger";
 import { usePluginContext } from "../../plugin";
 import { TenantTable } from "../table/TenantTable";
 
@@ -11,22 +15,76 @@ import style from "./details.module.scss";
 
 const cx = classNames.bind(style);
 
+type UserWithRole = { roles: string[] } & User;
+
 type TenantUsersProps = {
-  onFetch: () => Promise<{ users: User[] }>;
+  onFetch: () => Promise<{ users: UserWithRole[] }>;
 };
 
 export const TenantUsers: React.FC<TenantUsersProps> = ({ onFetch }) => {
   const { t } = usePluginContext();
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<UserWithRole[]>([]);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [currentUserDetails, setCurrentUserDetails] = useState<UserWithRole | null>(null);
+  const isCurrentUserAdmin = useMemo(() => currentUserDetails?.roles.includes(ROLES.ADMIN), [currentUserDetails]);
 
   const loadDetails = useCallback(async () => {
     const details = await onFetch();
-    setUsers(details.users);
+    // Show the users that have a valid role
+    setUsers(details.users.filter((user) => user.roles.length !== 0));
+
+    const userIdFromSession = await Session.getUserId();
+    setUserId(userIdFromSession);
+
+    // determine if the current user is admin or not.
+    const currentUserDetailsWithRole = details.users.find((user) => user.id === userIdFromSession);
+    setCurrentUserDetails(currentUserDetailsWithRole ?? null);
   }, [onFetch]);
 
   useEffect(() => {
     loadDetails();
   }, [loadDetails]);
+
+  const availableRoles = [
+    {
+      label: "Admin",
+      value: ROLES.ADMIN,
+    },
+    {
+      label: "Member",
+      value: ROLES.MEMBER,
+    },
+  ];
+
+  const getExtraComponent = useCallback(
+    (user: { roles: string[] } & User) => {
+      // It's safe to assume that they would have one role
+      const currentRole = user.roles[0] ?? ROLES.MEMBER;
+
+      const handleRoleChange = (newValue) => {
+        logDebugMessage(`Changing role for user to: ${newValue}`);
+      };
+
+      return (
+        <div>
+          <div className="role--input">
+            <SelectInput
+              id="role-select"
+              value={currentRole}
+              onChange={(e: any) => handleRoleChange(e.target.value)}
+              options={availableRoles}
+              disabled={!isCurrentUserAdmin}
+            />
+          </div>
+        </div>
+      );
+    },
+    [isCurrentUserAdmin],
+  );
+
+  if (!userId) {
+    return <div>Loading....</div>;
+  }
 
   return (
     <div className={cx("tenantDetailsSection")}>
@@ -41,6 +99,7 @@ export const TenantUsers: React.FC<TenantUsersProps> = ({ onFetch }) => {
                     <div className={cx("userEmail")}>{user.emails[0]}</div>
                   </div>
                 ),
+                extraComponent: getExtraComponent(user),
               }))}
             />
           </div>
