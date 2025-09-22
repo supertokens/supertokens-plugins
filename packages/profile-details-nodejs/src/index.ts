15402c6e2f52cdafbe55ed67946d42a85dd7dfdb
Victor Bojica <victor@niftylearning.io>
2025-09-22 16:03:55 +0300

    added basic exports

--- packages/profile-details-nodejs/src/index.ts
+++ packages/profile-details-nodejs/src/index.ts
@@ -1,4 +1,25 @@
 import { init } from "./plugin";
 import { PLUGIN_ID, PLUGIN_VERSION } from "./constants";
+import { SessionContainerInterface } from "supertokens-node/recipe/session/types";
+import { Implementation } from "./implementation";
+import { BaseFormFieldPayload } from "@supertokens-plugins/profile-details-shared";
 export { init, PLUGIN_ID, PLUGIN_VERSION };
-export default { init, PLUGIN_ID, PLUGIN_VERSION };
+
+export const getProfile = (userId: string, session: SessionContainerInterface, userContext?: Record<string, any>) => {
+  return Implementation.getInstanceOrThrow().getProfile(userId, session, userContext);
+};
+
+export const updateProfile = (
+  userId: string,
+  profile: BaseFormFieldPayload[],
+  session: SessionContainerInterface,
+  userContext?: Record<string, any>
+) => {
+  return Implementation.getInstanceOrThrow().updateProfile(userId, profile, session, userContext);
+};
+
+export const getSections = (session: SessionContainerInterface, userContext?: Record<string, any>) => {
+  return Implementation.getInstanceOrThrow().getSections(session, userContext);
+};
+
+export default { init, PLUGIN_ID, PLUGIN_VERSION, getProfile, updateProfile, getSections };
