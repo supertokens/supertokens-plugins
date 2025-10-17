fc9160606e1b167440908a959877f39b87544779
Deepjyoti Barman <deepjyoti@supertokens.com>
2025-10-17 19:05:17 +0530

    op: avoid duplicate import for error module scss

--- packages/tenant-enrollment-react/src/components/error/error-message.tsx
+++ packages/tenant-enrollment-react/src/components/error/error-message.tsx
@@ -1,7 +1,6 @@
 import classNames from "classnames/bind";
 import React from "react";
 
-import "./error.module.scss";
 import styles from "./error.module.scss";
 
 const cx = classNames.bind(styles);
