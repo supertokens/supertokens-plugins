5013e82c4f1a466ded9d0f6f510fc0b99ec600e4
Deepjyoti Barman <deepjyoti@supertokens.com>
2025-10-22 15:07:10 +0530

    fix: issue of CSS injection for error message wrapper

--- packages/tenant-enrollment-react/src/components/error/error-message.tsx
+++ packages/tenant-enrollment-react/src/components/error/error-message.tsx
@@ -1,14 +1,18 @@
-import classNames from "classnames/bind";
 import React from "react";
-
-import styles from "./error.module.scss";
-
-const cx = classNames.bind(styles);
 
 type ErrorMessageProps = {
   message: string;
 };
 
 export const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
-  return <div className={cx("errorMessageWrapper")}>{message}</div>;
+  return (
+    <div
+      style={{
+        color: "var(--semantic-colors-error-9)",
+        marginBottom: "12px",
+      }}
+    >
+      {message}
+    </div>
+  );
 };
